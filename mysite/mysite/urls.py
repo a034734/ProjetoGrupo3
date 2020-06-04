e4fe255b576fe910122b7e09371002c4f0b6c0e9
Rodrigo Mota <61556454+rodmot@users.noreply.github.com>
2020-06-04 11:04:29 +0100

    Add files via upload

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,14 +15,45 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from register import views as v
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
 
      # main will be the name of your app
     path('admin/', admin.site.urls),
-    path("register/", v.register, name="register"),
-    path('', include('main.urls')),
     path('', include("django.contrib.auth.urls")),
+    path("register/", v.register, name="register"),
+    path("profile/", v.profile, name="profile"),
+    path('password-reset/',
+        auth_views.PasswordResetView.as_view(
+        template_name='new_password/password_reset.html'
+        ),
+        name='password_reset'),
+    path('password-reset/done/',
+        auth_views.PasswordResetDoneView.as_view(
+        template_name='new_password/password_reset_done.html'
+        ),
+        name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+        template_name='new_password/password_reset_confirm.html'
+        ),
+        name="password_reset_confirm"),
+    path('password-reset-complete/',
+        auth_views.PasswordResetCompleteView.as_view(
+        template_name='new_password/password_reset_complete.html'
+        ),
+        name="password_reset_complete"),
+    path('', include('main.urls')),
+
+
+
+
 ]
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
