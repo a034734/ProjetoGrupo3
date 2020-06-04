548ebd276de60ccb63053c758fb72d1fba4bb1e0
Rodrigo Mota <61556454+rodmot@users.noreply.github.com>
2020-06-04 11:05:47 +0100

    Add files via upload

--- mysite/register/views.py
+++ mysite/register/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
+from .forms import RegisterForm, UserUpdateForm, ProfileUpdateForm
 
 # Create your views here.
 
@@ -14,5 +14,28 @@ def register(response):
 
     return render(response, "register/register.html", {"form":form})
 
+
+
 def login(response):
     return render(response,'registration/login.html')
+
+
+
+def profile(request):
+    if request.method == "POST":
+        upd_form = UserUpdateForm(request.POST, instance=request.user)
+        pf_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if upd_form.is_valid() and pf_form.is_valid():
+            upd_form.save()
+            pf_form.save()
+
+    else:
+        upd_form = UserUpdateForm(instance=request.user)
+        pf_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'upd_form': upd_form,
+        'pf_form': pf_form
+    }
+    return render(request, "profile/profile.html", context)
